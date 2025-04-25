b08e0f8f3a459a9f4431ac91961d1ac773656b7d
cignoir <cignoir@gmail.com>
2025-04-25 22:59:45 +0900

    batをバージョン管理

---
+++ unpack_all.bat
@@ -0,0 +1,8 @@
+python unpack.py bin/AnimInfo_024.bin bin/Anim_024.bin output/anim
+python unpack.py bin/ClumpInfo_128.bin bin/Clump_128.bin output/clump
+python unpack.py bin/IdTblInfo_002.bin bin/IdTbl_002.bin output/idtbl
+python unpack.py bin/PrtInfo_011.bin bin/Prt_011.bin output/prt
+python unpack.py bin/SEInfo_002.bin bin/SE_002.bin output/se
+python unpack.py bin/TexInfo_193.bin bin/Tex_193.bin output/tex
+python unpack.py bin/WindowInfo_101.bin bin/Window_101.bin output/window
+python extract_textures.py bin/TexInfo_193.bin bin/Tex_193.bin output/png
